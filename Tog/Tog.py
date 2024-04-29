ac851a4dce5ba0f61ae608c02add82ba8864767b
JuYoung Kim <robolab1902@naver.com>
2024-04-29 17:58:15 +0900

    선제적으로 페이지 업로드

---
+++ Tog/Tog.py
@@ -0,0 +1,6 @@
+
+
+while True
+  str = input();
+  print("str : ", str);
+  
---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/info/terms', methods=['GET', 'POST'])
+def terms():
+    return render_template('terms.html')
+
+@app.route('/info/service', methods=['GET', 'POST'])
+def service():
+    return render_template('service.html')
+
+@app.route('/info/age', methods=['GET', 'POST'])
+def age():
+    return render_template('age.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
